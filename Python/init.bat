cc5b5fcf9bfa15de0e24ae88a1df34c406f5fe25
MrLYC <imyikong@gmail.com>
2015-12-08 14:13:25 +0800

    update

--- Python/init.bat
+++ Python/init.bat
@@ -1,3 +1,8 @@
 pip install --no-index --find-links=wheelhouse -r requirements-init.txt
+
+cd virtualenvwrapper-win-1.2.1
+python setup.py install
+cd ../
+
 mkvirtualenv djangogirls
 pip install --no-index --find-links=wheelhouse -r requirements-dev.txt
